a039e150ba6e2a41d48557df2d49db6b37a4ec41
Mini_App_ComplexeSportif <islemtk@gmail.com>
2025-01-05 20:01:00 +0100

    Add backend files for Spring Boot

---
+++ backend/Portail-RH1/Portail-RH/src/main/java/com/example/Portail_RH/repository/userrepo.java
@@ -0,0 +1,7 @@
+package com.example.Portail_RH.repository;
+
+import com.example.Portail_RH.model.user;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface userrepo extends JpaRepository <user,Long>{
+}
